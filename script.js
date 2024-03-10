05564cf54bd4d4a064664585d16a864d748ac51e
abdulahad0110 <abdulahad200325@gmail.com>
2024-03-11 01:13:45 +0500

    javaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+ // Question 1
+ alert("Hello World")
+ // Question 2
+ alert("Error! Please enter a valid password")
+ // Question 3
+ alert("Wellcome to JS Land \nHappy Coding!")
+ // Question 4
+ alert("Wellcome to JS Land")
+ alert("Happy Coding!")
+ // Question 5
+ alert("Hello... I can run JS through my brower's console")
